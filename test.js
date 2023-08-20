0ca9d316be3ddeee24d03dda373dbafd8497237d
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2023-08-20 08:15:17 -0700

    Update test.js to 2025

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ const year = document.querySelector("#year");
 document.addEventListener("DOMContentLoaded", () => {
   const date = new Date();
   const currentYear = date.getFullYear();
-  year.textContent = currentYear;
+  year.textContent = "2025";
 });
