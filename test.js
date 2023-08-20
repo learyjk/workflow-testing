25ac39f8f09269474ef7fea62160dbfb5864dbee
Keegan Leary <35752455+learyjk@users.noreply.github.com>
2023-08-20 08:04:12 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+const year = document.querySelector("#year");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const date = new Date();
+  const currentYear = date.getFullYear();
+  year.textContent = currentYear;
+});
